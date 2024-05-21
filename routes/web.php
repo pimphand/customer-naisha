219dcf506af7fae15e910d6fee924a9cb31a67ec
pimphand <faisaldwiki69@gmail.com>
2024-05-21 10:54:53 +0700

    add

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,10 @@ Route::get('/', function () {
 Route::get('/checkout', function () {
     return view('cart');
 })->name('checkout');
+
+Route::get('/pull-asd', function () {
+    $output = shell_exec('git pull');
+
+    // Menampilkan output dari perintah git pull
+    echo "<pre>$output</pre>";
+});
