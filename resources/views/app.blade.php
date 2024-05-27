a97ed60c76029df9e581c6fc1cb20aa1d9f67e94
pimphand <faisaldwiki69@gmail.com>
2024-05-27 12:22:13 +0700

    add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,10 +4,13 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Mazia - Clean Minimal eCommerce HTML5 Template</title>
+    <title>Naisha Inspirasi Muslimah</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets')}}/img/favicon.ico">
+    <link rel="icon" type="image/png"
+        href="https://s3-ap-southeast-1.amazonaws.com/technoidea%2Fgallery/favicon-25464-cc3b915a-499c-41ed-bd52-39d1dd57e610.png" />
+    {{--
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets')}}/img/favicon.ico"> --}}
     <!-- Place favicon.ico in the root directory -->
     <!-- CSS here -->
     <link rel="stylesheet" href="{{asset('assets')}}/css/bootstrap.min.css">
