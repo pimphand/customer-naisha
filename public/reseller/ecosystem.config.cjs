ad5b03336da87062bbb0a852094b24299e56a197
pimphand <faisaldwiki69@gmail.com>
2024-08-07 06:56:58 +0700

    fixing

--- public/reseller/ecosystem.config.cjs
+++ public/reseller/ecosystem.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
       name: 'react-app',
       script: 'serve',
       args: '-s build',
-      cwd: '/var/www/html/customer-naisha/reseller',
+      cwd: '/var/www/html/customer-naisha/public/reseller',
       env: {
         PORT: 302 // You can specify the port number if needed
       }
